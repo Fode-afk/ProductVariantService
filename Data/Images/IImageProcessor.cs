f7a2e459760487ec928789c984f79dd2ab515f24
Sviataslau <sastisin@gmail.com>
2025-06-12 14:50:51 +0300

    Image workflow deletion

--- Data/Images/IImageProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ProductService.Data.Images
-{
-    public interface IImageProcessor
-    {
-        Task<bool> ProcessImageAsync(string base64, string productId, string fileName);
-        Task<string> ConvertImageAsync(byte[] bytes);
-        Task<bool> DeleteImageAsync(string imageURL);
-    }
-}
