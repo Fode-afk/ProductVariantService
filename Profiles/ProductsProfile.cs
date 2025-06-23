809c9f31af14bc4757d0dd451484666de3adfa5d
FanifffsDev <fanifffs.dev@gmail.com>
2025-06-23 21:25:28 +0300

    small fixes

--- Profiles/ProductsProfile.cs
+++ Profiles/ProductsProfile.cs
@@ -1,8 +1,7 @@
 ﻿using AutoMapper;
-using Google.Protobuf.WellKnownTypes;
 using ProductService.Models;
 using ProductService.Protos;
-using System.Globalization;
+using ProductService.Utils;
 
 namespace ProductService.Profiles
 {
@@ -13,8 +12,8 @@ namespace ProductService.Profiles
             CreateMap<ProductGrpc, Product>()
              .ForMember(dest => dest.Attributes, opt => opt.MapFrom(src => src.Attributes.ToList()))
              .ForMember(dest => dest.ImageURLs, opt => opt.MapFrom(src => src.ImageURLs.ToList()))
-             .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => ParseDate(src.CreatedAt)))
-             .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => ParseDate(src.UpdatedAt)));
+             .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTimeUtil.ParseDate(src.CreatedAt)))
+             .ForMember(dest => dest.UpdatedAt, opt => opt.MapFrom(src => DateTimeUtil.ParseDate(src.UpdatedAt)));
 
             CreateMap<Product, ProductGrpc>()
                 .ForMember(dest => dest.Attributes, opt => opt.MapFrom(src => src.Attributes))
@@ -29,17 +28,6 @@ namespace ProductService.Profiles
 
             CreateMap<UpdateProductRequest, Product>()
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
-        }
-
-        private DateTimeOffset? ParseDate(string date)
-        {
-            DateTimeOffset res;
-            if (DateTimeOffset.TryParseExact(date, "yyyy-MM-ddTHH:mm:ss.fffK",
-                CultureInfo.InvariantCulture, DateTimeStyles.None, out res))
-            {
-                return res;
-            }
-            return null;
         }
     }
 }
--- Utils/DateTimeUtil.cs
+++ Utils/DateTimeUtil.cs
@@ -1,4 +1,6 @@
-﻿namespace ProductService.Utils
+﻿using System.Globalization;
+
+namespace ProductService.Utils
 {
     public static class DateTimeUtil
     {
@@ -7,6 +9,17 @@
             TimeZoneInfo timeZone = TimeZoneInfo.FindSystemTimeZoneById(config["Time"]);
             DateTime localDateTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZone);
             return new DateTimeOffset(localDateTime, timeZone.GetUtcOffset(localDateTime));
+        }
+
+        public static DateTimeOffset? ParseDate(string date)
+        {
+            DateTimeOffset res;
+            if (DateTimeOffset.TryParse(date,
+                CultureInfo.InvariantCulture, DateTimeStyles.None, out res))
+            {
+                return res;
+            }
+            return null;
         }
     }
 }
