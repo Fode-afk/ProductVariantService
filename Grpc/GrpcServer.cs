d2033e42267948df08dcbb9116c8faa127b34f8e
Sviataslau <sastisin@gmail.com>
2025-08-19 21:06:25 +0300

    reviews proto & logic added

--- Grpc/GrpcServer.cs
+++ Grpc/GrpcServer.cs
@@ -9,17 +9,19 @@ using ProductService.Grpc.Validators;
 using ProductService.Models;
 using ProductService.Protos;
 using ProductService.Utils;
+using ReviewService.Protos;
 
 namespace ProductService.Grpc
 {
     public class GrpcServer(IProductRepo productRepo, IMapper mapper, IMessageBusClient messageBusClient,
-        IConfiguration config, ILogger logger) : GrpcProducts.GrpcProductsBase
+        IConfiguration config, ILogger logger, GrpcReviews.GrpcReviewsClient reviewsClient) : GrpcProducts.GrpcProductsBase
     {
         private readonly IProductRepo _productRepo = productRepo;
         private readonly IMapper _mapper = mapper;
         private readonly IMessageBusClient _messageBusClient = messageBusClient;
         private readonly IConfiguration _config = config;
         private readonly ILogger _logger = logger;
+        private readonly GrpcReviews.GrpcReviewsClient _reviewsClient = reviewsClient;
 
         private readonly IValidator<CreateProductRequest> _createProductValidator = new CreateProductValidator();
         private readonly IValidator<GetProductRequest> _getProductValidator = new GetProductValidator();
@@ -35,9 +37,9 @@ namespace ProductService.Grpc
 
             if (!res.success)
             {
-                return new GetProductsByIdsResponse { Status = new StatusResponse { Status = res.success, Reason = res.message } };
+                return new GetProductsByIdsResponse { Status = new Protos.StatusResponse { Status = res.success, Reason = res.message } };
             }
-            return new GetProductsByIdsResponse { Status = new StatusResponse { Status = res.success }, Products = { _mapper.Map<IEnumerable<ProductGrpc>>(res.Value) } };
+            return new GetProductsByIdsResponse { Status = new Protos.StatusResponse { Status = res.success }, Products = { _mapper.Map<IEnumerable<ProductGrpc>>(res.Value) } };
         }
 
         public override async Task<GetAllProductsByOwnerIdResponse> GetAllProductsByOwnerId(GetAllProductsByOwnerIdRequest request, ServerCallContext context)
@@ -55,12 +57,12 @@ namespace ProductService.Grpc
 
             if (!res.success)
             {
-                return new GetAllProductsByOwnerIdResponse { Status = new StatusResponse { Status = res.success, Reason = res.message} };
+                return new GetAllProductsByOwnerIdResponse { Status = new Protos.StatusResponse { Status = res.success, Reason = res.message} };
             }
 
             return new GetAllProductsByOwnerIdResponse
             {
-                Status = new StatusResponse { Status = true },
+                Status = new Protos.StatusResponse { Status = true },
                 Products = { _mapper.Map<IEnumerable<ProductGrpc>>(res.Value) }
             };
         }
@@ -80,17 +82,17 @@ namespace ProductService.Grpc
 
             if (!res.success)
             {
-                return new GetProductResponse { Status = new StatusResponse { Status = res.success, Reason = res.message } };
+                return new GetProductResponse { Status = new Protos.StatusResponse { Status = res.success, Reason = res.message } };
             }
 
             return new GetProductResponse
             {
-                Status = new StatusResponse { Status = res.success },
+                Status = new Protos.StatusResponse { Status = res.success },
                 Product = _mapper.Map<ProductGrpc>(res.Value)
             };
         }
 
-        public override async Task<StatusResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> CreateProduct(CreateProductRequest request, ServerCallContext context)
         {
             _logger.Log($"\"CreateProduct\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -110,17 +112,33 @@ namespace ProductService.Grpc
 
             var res = await _productRepo.CreateProductAsync(product);
             
-            if (res.success)
-            {
-                var productPub = _mapper.Map<ProductPublishedDto>(product);
+            if (!res.success)
+                return new Protos.StatusResponse { Status = false, Reason = res.message };
 
-                await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductPublished, [ServicesEnum.SEARCH_SERVICE]);
+            try
+            {
+                var ratingRes = await _reviewsClient.CreateRatingAsync(new CreateRatingRequest { Id = product.ProductId });
+
+                if (!ratingRes.Status)
+                {
+                    await _productRepo.DeleteProductAsync(product.ProductId);
+                    return new Protos.StatusResponse { Status = false, Reason = res.message };                 
+                }
+            }
+            catch (Exception ex)
+            {
+                await _productRepo.DeleteProductAsync(product.ProductId);
+                return new Protos.StatusResponse { Status = false, Reason = ex.Message };
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            var productPub = _mapper.Map<ProductPublishedDto>(product);
+
+            await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductPublished, [ServicesEnum.SEARCH_SERVICE]);           
+
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
-        public override async Task<StatusResponse> UpdateProductById(UpdateProductRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> UpdateProductById(UpdateProductRequest request, ServerCallContext context)
         {
             _logger.Log($"\"UpdateProductById\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -146,10 +164,10 @@ namespace ProductService.Grpc
                 await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductUpdatePublished, [ServicesEnum.SEARCH_SERVICE]);
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
-        public override async Task<StatusResponse> SetCanBeOrdered(SetCanBeOrderedRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> SetCanBeOrdered(SetCanBeOrderedRequest request, ServerCallContext context)
         {
             _logger.Log($"\"SetCanBeOrdered\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");          
             var localTime = DateTimeUtil.GetCurrentTimeFormatted(_config);
@@ -163,10 +181,10 @@ namespace ProductService.Grpc
                 await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductUpdatePublished, [ServicesEnum.SEARCH_SERVICE]);
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
-        public override async Task<StatusResponse> DeleteProductById(DeleteProductRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> DeleteProductById(DeleteProductRequest request, ServerCallContext context)
         {
             _logger.Log($"\"DeleteProductById\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -209,15 +227,15 @@ namespace ProductService.Grpc
                 }
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
-        public override async Task<StatusResponse> ProductExists(ProductExistsRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> ProductExists(ProductExistsRequest request, ServerCallContext context)
         {
             _logger.Log($"\"ProductExists\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
             var res = await _productRepo.ProductExistsAsync(request.ProductId);
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
         public override async Task<GetParentCardIdResponse> GetParentCardId(GetParentCardIdRequest request, ServerCallContext context)
@@ -228,13 +246,13 @@ namespace ProductService.Grpc
 
             if (res.Value == "")
             { 
-                return new GetParentCardIdResponse {Status = new StatusResponse { Status = res.success, Reason = res.message } };
+                return new GetParentCardIdResponse {Status = new Protos.StatusResponse { Status = res.success, Reason = res.message } };
             }
 
-            return new GetParentCardIdResponse {Status = new StatusResponse { Status = res.success, Reason = res.message }, ParentCardId = res.Value };
+            return new GetParentCardIdResponse {Status = new Protos.StatusResponse { Status = res.success, Reason = res.message }, ParentCardId = res.Value };
         }
 
-        public override async Task<StatusResponse> UpdateParentCardId(UpdateParentCardIdRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> UpdateParentCardId(UpdateParentCardIdRequest request, ServerCallContext context)
         {
             _logger.Log($"\"UpdateParentCardId\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -253,7 +271,7 @@ namespace ProductService.Grpc
                 await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductUpdateParentCardIdPublished, [ServicesEnum.SEARCH_SERVICE]);
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
         public override async Task<GetOwnerIdResponse> GetOwnerId(GetOwnerIdRequest request, ServerCallContext context)
@@ -261,7 +279,7 @@ namespace ProductService.Grpc
             var res = await _productRepo.GetOwnerIdAsync(request.ProductId);
 
             return new GetOwnerIdResponse {
-                Status = new StatusResponse
+                Status = new Protos.StatusResponse
                 { 
                     Status = res.success,
                     Reason = res.message
@@ -269,7 +287,7 @@ namespace ProductService.Grpc
                 OwnerId = res.Value
             };
         }
-        public override async Task<StatusResponse> AddAttributesToProduct(AddAttributesToProductRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> AddAttributesToProduct(AddAttributesToProductRequest request, ServerCallContext context)
         {
             _logger.Log($"\"AddAttributesToProduct\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -288,10 +306,10 @@ namespace ProductService.Grpc
                 await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductUpdatePublished, [ServicesEnum.SEARCH_SERVICE]);
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
 
-        public override async Task<StatusResponse> DeleteAttributesFromProduct(DeleteAttributesFromProductRequest request, ServerCallContext context)
+        public override async Task<Protos.StatusResponse> DeleteAttributesFromProduct(DeleteAttributesFromProductRequest request, ServerCallContext context)
         {
             _logger.Log($"\"DeleteAttributesFromProduct\" with params {request.ToJson()} has noticed. Caller: {context.Peer}");
 
@@ -310,7 +328,7 @@ namespace ProductService.Grpc
                 await _messageBusClient.PublishGenericEvent(productPub, EventType.ProductUpdatePublished, [ServicesEnum.SEARCH_SERVICE]);            
             }
 
-            return new StatusResponse { Status = res.success, Reason = res.message };
+            return new Protos.StatusResponse { Status = res.success, Reason = res.message };
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@ using ProductService.AsyncDataServices;
 using ProductService.Data;
 using ProductService.EventProcessing;
 using ProductService.Grpc;
+using ProductService.Protos;
 using ProductService.Settings;
+using ReviewService.Protos;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +41,11 @@ builder.Services.AddSingleton<ILogger>(provider =>
 {
     var logPath = Path.Combine("Logs", $"{DateTime.UtcNow:yyyy-MM-dd HH-mm-ss}.log");
     return new Logger(logPath);
+});
+
+builder.Services.AddGrpcClient<GrpcReviews.GrpcReviewsClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcReview"]);
 });
 
 var app = builder.Build();
